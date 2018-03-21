8014ef746eae6d9eedeacaae17ecf8ae25f4ab91
atlantis26 <liuming_51@sina.cn>
2018-03-21 17:24:19 +0800

    python知识与作业

---
+++ 2、python基础/example/作业4_工资管理系统/作业4_工资管理系统.py
@@ -0,0 +1,29 @@
+# coding:utf-8
+
+
+def query():
+    pass
+
+
+def modify():
+    pass
+
+
+def insert():
+    pass
+
+
+def exit_system():
+    pass
+
+
+def work_flow():
+    pass
+
+
+def console():
+    pass
+
+
+if __name__ == "__main__":
+    console()
