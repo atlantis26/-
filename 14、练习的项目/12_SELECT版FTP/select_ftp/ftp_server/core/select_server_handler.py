ebe520d7824fa0cb49b564be8a6b94128e4d4753
atlantis26 <liuming_51@sina.cn>
2018-05-28 17:58:00 +0800

    python知识与作业

--- 14、练习的项目/12_SELECT版FTP/select_ftp/ftp_server/core/select_server_handler.py
+++ 14、练习的项目/12_SELECT版FTP/select_ftp/ftp_server/core/select_server_handler.py
@@ -9,6 +9,7 @@ import os
 import socket
 import select
 import queue
+import threading
 
 
 class SelectSocketServer(object):
@@ -24,9 +25,13 @@ class SelectSocketServer(object):
         # 将socket服务端加入到检测列表inputs中
         inputs = [self.server]
         outputs = []
+
         while True:
             # 初始执行检测，只有server处于检测列表中
-            readable, writeable, exceptional = select.select(inputs, outputs, inputs)
+            try:
+                readable, writeable, exceptional = select.select(inputs, outputs, inputs, 1)
+            except Exception:
+                break
             for r in readable:
                 if r is self.server:
                     # 检测到server变活跃则表示有新连接过来，则将新连接conn加入到检测列表inputs中
@@ -39,17 +44,19 @@ class SelectSocketServer(object):
                     else:
                         ftp = FtpHandler(r)
                         self.conn_cache[r] = ftp
-                    cmd, response = ftp.get_response()
-                    ftp.sendall_data(cmd, response)
-                    # personal_ftp = {"obj": ftp, "cmd": cmd, "response": response}
-                    # self.queue_dict[r].put(personal_ftp)
-                    # outputs.append(r)  # 放入需返回数据的连接的列表中
+                    # 创建子线程异步处理单个socket的数据请求的操作，由于传输大文件内容的柱塞问题，选择子线程异步处理请求
+                    que = self.queue_dict[r]
+                    req_thread = RequestThread(r, ftp, que, outputs)
+                    req_thread.start()
 
-            # 要返回给客户端的连接列表
-            # for w in outputs:
-            #     personal_ftp = self.queue_dict[w].get()
-            #     personal_ftp["obj"].sendall_data(personal_ftp["cmd"], personal_ftp["response"])
-            #     outputs.remove(w)
+            for w in outputs:
+                # 创建子线程异步处理单个socket的请求响应数据的发送操作，由于传输大文件内容会柱塞问题，子线程异步处理请求
+                ftp = self.conn_cache[w]
+                que = self.queue_dict[w]
+                rsp_thread = ResponseThread(w, ftp, que)
+                rsp_thread.start()
+                # 开启异步处理后，直接将socket连接句柄从outputs列表中移出
+                outputs.remove(w)
 
             for e in exceptional:
                 if e in outputs:
@@ -57,6 +64,37 @@ class SelectSocketServer(object):
                 inputs.remove(e)
                 del self.queue_dict[e]
                 del self.conn_cache[e]
+
+
+class RequestThread(threading.Thread):
+    def __init__(self, request, ftp_obj, que, outputs):
+        self.request = request
+        self.ftp_obj = ftp_obj
+        self.que = que
+        self.outputs = outputs
+        threading.Thread.__init__(self)
+
+    def run(self):
+        cmd, data = self.ftp_obj.get_response()
+        # 处理完请求后，返回数据放入到队列中
+        response_data = (cmd, data)
+        self.que.put(response_data)
+        # 处理完请求后，将需要返回响应数据的请求放入到outputs列表中
+        self.outputs.append(self.request)
+
+
+class ResponseThread(threading.Thread):
+    def __init__(self, request, ftp_obj, que):
+        self.request = request
+        self.ftp_obj = ftp_obj
+        self.que = que
+        threading.Thread.__init__(self)
+
+    def run(self):
+        # 取到请求返回数据
+        cmd, data = self.que.get()
+        # 执行请求放回数据的send发送
+        self.ftp_obj.sendall_data(cmd, data)
 
 
 class FtpHandler(object):
@@ -110,14 +148,15 @@ class FtpHandler(object):
 
     def _receive_file_data(self, file_size):
         """接收文件数据，暂时存放在临时文件内"""
-        now = datetime.now()
-        time_stamp = now.strftime("%Y-%m-%d_%H%M%S")
-        temp_file = os.path.join(DB_TEMP, "temp_file_{0}_{1}".format(file_size, time_stamp))
-        receive_size = 0
-        f = open(temp_file, "wb")
-        while True:
-            diff = file_size - receive_size
-            try:
+        try:
+            self.request.setblocking(True)
+            now = datetime.now()
+            time_stamp = now.strftime("%Y-%m-%d_%H%M%S")
+            temp_file = os.path.join(DB_TEMP, "temp_file_{0}_{1}".format(file_size, time_stamp))
+            receive_size = 0
+            f = open(temp_file, "wb")
+            while True:
+                diff = file_size - receive_size
                 if diff > 1024:
                     data = self.request.recv(1024)
                 elif diff == 0:
@@ -125,29 +164,28 @@ class FtpHandler(object):
                     break
                 else:
                     data = self.request.recv(diff)
-            except BlockingIOError:
-                continue
-            f.write(data)
-            f.flush()
-            receive_size += len(data)
-
+                f.write(data)
+                f.flush()
+                receive_size += len(data)
+        finally:
+            self.request.setblocking(False)
         return temp_file
 
     def sendall_data(self, cmd, response):
         """发送请求响应数据,对下载文件请求（cmd=get）做单独处理"""
-        response_body = json.dumps(response.__dict__).encode("utf-8")
-        self.request.sendall(response_body)
-        if cmd == "get" and response.code == 200:
-            file_path = response.data.pop("file_path")
-            seek_size = response.data.get("seek_size")
-            with open(file_path, "rb") as f:
-                f.seek(seek_size)
-                while True:
-                    data = f.read(1024)
-                    if not data:
-                        break
-                    try:
+        try:
+            response_body = json.dumps(response.__dict__).encode("utf-8")
+            self.request.sendall(response_body)
+            if cmd == "get" and response.code == 200:
+                self.request.setblocking(True)
+                file_path = response.data.pop("file_path")
+                seek_size = response.data.get("seek_size")
+                with open(file_path, "rb") as f:
+                    f.seek(seek_size)
+                    while True:
+                        data = f.read(1024)
+                        if not data:
+                            break
                         self.request.sendall(data)
-                    except BlockingIOError:
-                        print("BlockingIOError")
-                        continue
+        finally:
+            self.request.setblocking(False)
