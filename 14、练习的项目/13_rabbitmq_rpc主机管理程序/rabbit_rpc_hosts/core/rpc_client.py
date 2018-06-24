08994598a8288d961061cca3c2db07c6781f1336
atlantis26 <liuming_51@sina.cn>
2018-06-25 07:57:06 +0800

    python知识与作业

--- 14、练习的项目/13_rabbitmq_rpc主机管理程序/rabbit_rpc_hosts/core/rpc_client.py
+++ 14、练习的项目/13_rabbitmq_rpc主机管理程序/rabbit_rpc_hosts/core/rpc_client.py
@@ -1,36 +1,67 @@
 import pika
 import uuid
+import json
 
 
-class FibonacciRpcClient(object):
-    def __init__(self):
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+class RpcClient(object):
+    def __init__(self, host, port, virtual_host, username, password):
+        self.task_id = None
+        self.response = None
+        # 声明mq连接用户、连接实例、声明使用管道
+        credentials = pika.PlainCredentials(username, password)
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host,
+                                                                            port=port,
+                                                                            virtual_host=virtual_host,
+                                                                            credentials=credentials))
         self.channel = self.connection.channel()
+        # 生成一个随机队列callback_queue,用于告诉服务端将请求结果发送到哪里
         result = self.channel.queue_declare(exclusive=True)
         self.callback_queue = result.method.queue
-        self.channel.basic_consume(self.on_response, no_ack=True, queue=self.callback_queue)
+        self.channel.basic_consume(self.get_response, queue=self.callback_queue)
+        self.channel.queue_declare(queue='rpc_queue')
 
-    def on_response(self, ch, method, props, body):
-        if self.corr_id == props.correlation_id:
+    def get_response(self, ch, method, props, body):
+        print(props.__dict__, body)
+        if self.task_id == props.correlation_id:
             self.response = body
+        return body
 
-    def call(self, n):
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
+    def run_cmd(self, cmds):
+        task_id = str(uuid.uuid4())
+        req_data = {"task_id": task_id, "check_task": False, "cmd": cmds}
+        req_data = json.dumps(req_data)
+        properties = pika.BasicProperties(reply_to=self.callback_queue, correlation_id=task_id)
         self.channel.basic_publish(exchange='',
                                    routing_key='rpc_queue',
-                                   properties=pika.BasicProperties(
-                                       reply_to=self.callback_queue,
-                                       correlation_id=self.corr_id,
-                                   ),
-                                   body=str(n))
-        while self.response is None:
-            self.connection.process_data_events()  # 非阻塞版的start_consume,
-        return int(self.response)
+                                   properties=properties,
+                                   body=req_data)
+        self.connection.process_data_events()  # 非阻塞发送请求
+        return task_id
+
+    def check_task(self, task_id):
+        req_data = {"task_id": task_id, "check_task": True}
+        req_data = json.dumps(req_data)
+        properties = pika.BasicProperties(reply_to=self.callback_queue, correlation_id=task_id)
+        print(self.callback_queue)
+        self.channel.basic_publish(exchange='',
+                                   routing_key=self.callback_queue,
+                                   properties=properties,
+                                   body=req_data)
+        # self.connection.process_data_events()  # 非阻塞发送请求
+        self.channel.start_consuming()
+
+        return self.response
+
+    def close(self):
+        self.connection.close()
 
 
-fibonacci_rpc = FibonacciRpcClient()
+if __name__ == "__main__":
+    client = RpcClient("localhost", 5672, "/", "alex", "123456")
+    # task_id = client.run_cmd("ls,pwd")
+    # print(task_id)
+    # rsp = client.check_task(task_id)
+    rsp = client.check_task("082b61a0-7ed4-42a2-bcb8-d48e03b0c389")
+    print(rsp)
+    client.close()
 
-print(" [x] Requesting fib(30)")
-response = fibonacci_rpc.call(30)
-print(" [.] Got %r" % response)
--- 14、练习的项目/13_rabbitmq_rpc主机管理程序/rabbit_rpc_hosts/core/rpc_server.py
+++ 14、练习的项目/13_rabbitmq_rpc主机管理程序/rabbit_rpc_hosts/core/rpc_server.py
@@ -1,38 +1,56 @@
 # _*_coding:utf-8_*_
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-    host='localhost'))
 
-channel = connection.channel()
+class RpcServer(object):
+    def __init__(self, host, port, virtual_host, username, password):
+        # 声明mq连接用户、连接实例、声明使用管道、声明使用的队列名称
+        credentials = pika.PlainCredentials(username, password)
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=host,
+                                                                       port=port,
+                                                                       virtual_host=virtual_host,
+                                                                       credentials=credentials))
+        self.channel = connection.channel()
+        self.channel.queue_declare(queue='rpc_queue')
 
-channel.queue_declare(queue='rpc_queue')
+    @staticmethod
+    def handler(req_data):
+        """
+        :param req_data: 请求数据，1个或多个可执行的命令，以逗号连接
+        :return: 请求返回结果，命令的执行结果
+        """
+        return req_data + b"response1"
+
+    def get_response(self, ch, method, props, body):
+        """
+        :param ch: 队列管道channel对象
+        :param method:请求方法对象
+        :param props:请求属性对象
+        :param body:请求数据
+        :return: 请求处理结果
+        """
+        # 处理请求
+        response = self.handler(body)
+        # 请求返回数据通过客户端指定的队列（reply_to）返回给客户端
+        response_queue = props.reply_to
+        print(response_queue)
+        # 客户端发送的请求task_id
+        task_id = props.correlation_id
+        print(task_id, body)
+        # 执行推送消息
+        ch.basic_publish(exchange='',
+                         routing_key=response_queue,
+                         properties=pika.BasicProperties(correlation_id=task_id),
+                         body=response)
+        # 确认推送的消息被处理完成ack
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def run(self):
+        self.channel.basic_consume(self.get_response, queue='rpc_queue')
+        self.channel.start_consuming()
 
 
-def fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
+if __name__ == "__main__":
 
-
-def on_request(ch, method, props, body):
-    n = int(body)
-
-    print(" [.] fib(%s)" % n)
-    response = fib(n)
-
-    ch.basic_publish(exchange='',
-                     routing_key=props.reply_to,
-                     properties=pika.BasicProperties(correlation_id=props.correlation_id),
-                     body=str(response))
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-
-
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(on_request, queue='rpc_queue')
-
-print(" [x] Awaiting RPC requests")
-channel.start_consuming()
+    server = RpcServer("localhost", 5672, "/", "alex", "123456")
+    server.run()
