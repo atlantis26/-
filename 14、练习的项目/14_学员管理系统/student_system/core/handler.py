7da2cc84dbb68b6f2b96210c42e93ecfc660d9b7
atlantis26 <liuming_51@sina.cn>
2018-07-02 17:52:44 +0800

    python知识与作业

--- 14、练习的项目/14_学员管理系统/student_system/core/handler.py
+++ 14、练习的项目/14_学员管理系统/student_system/core/handler.py
@@ -1,11 +1,24 @@
 #  coding:utf-8
 from db.db_handler import DatabaseHandler
 from core.utils import SomeError, ResponseData
+import logging
+
+logger = logging.getLogger("system.handler")
 
 
 def login(account, password):
-    user = DatabaseHandler.query_user_by_account(account, password)
-    if not user:
-        raise SomeError(u"用户账号或密码错误")
-    return user.__dict__
+    try:
+        user = DatabaseHandler.query_user_by_account(account, password)
+        if not user:
+            raise SomeError(u"用户账号或密码错误")
+        code = 200
+        msg = "用户登录验证成功"
+        data = user.__dict__
+    except SomeError as e:
+        code = 400
+        msg = "用户登录验证失败，详情：{0}".format(str(e))
+        data = None
+    logger.debug(ResponseData(code, msg, data).__dict__)
+
+    return ResponseData(code, msg, data)
 
--- 14、练习的项目/14_学员管理系统/student_system/core/main.py
+++ 14、练习的项目/14_学员管理系统/student_system/core/main.py
@@ -2,41 +2,25 @@
 from core.views.student_view import StudentView
 from core.views.teacher_view import TeacherView
 from core.views.manager_view import MangerView
-from db.db_handler import query_user_by_account
+from core.handler import login
 
 
-
-def user_views():
-    try:
-        user_info = login()
-
-        views = {"1": StudentView,
-                 "2": TeacherView,
-                 "3": MangerView}
-        role_id = input(u"请输入用户身份类型编号（1.学员；2.讲师；3.管理员）： ").strip()
-        if role_id not in views:
-            raise Exception(u"输入身份类型{0}不存在，请核对后再试".format(role_id))
-        name = input(u"请输入账号：").strip()
-        password = input(u"请输入密码：").strip()
-        views[role_id](name, password, role_id)
-    except Exception as e:
-        print(str(e))
+def views(username, password, role_id):
+    items = {"1": MangerView, "2": TeacherView, "3": StudentView}
+    items[role_id](username, password, role_id)
 
 
 def console():
+    print(u"欢迎访问学员管理系统，请先登录。。。")
     while True:
-        msg = u"""------------欢迎访问学员管理系统---------------
-            您可以选择如下操作：
-                <\033[36;1m1\033[0m>.用户登录                     <\033[36;1m2\033[0m>.退出系统
-        """
-        print(msg)
-        num = input(u"请输入您选择的操作编号：").strip()
-        if num == "1":
-            user_views()
-        elif num == "2":
-            exit()
+        username = input(u"请输入登录账号：").strip()
+        password = input(u"请输入登录密码：").strip()
+        rsp = login(username, password)
+        if rsp.code == 200:
+            role_id = rsp.data["role_id"]
+            views(username, password, role_id)
         else:
-            print(u"输入的操作项编号{0}不存在，请核对后再试".format(num))
+            print(rsp.msg)
 
 
 if __name__ == "__main__":
