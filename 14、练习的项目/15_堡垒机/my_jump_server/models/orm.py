2f1a415a224e35178887df8382a4534544e48b4d
atlantis26 <liuming_51@sina.cn>
2018-08-02 17:16:50 +0800

    python知识与作业

--- 14、练习的项目/15_堡垒机/my_jump_server/models/orm.py
+++ 14、练习的项目/15_堡垒机/my_jump_server/models/orm.py
@@ -1,59 +1,120 @@
-# coding:utf-8
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, UniqueConstraint, Table
-from sqlalchemy.orm import relationship
-from sqlalchemy_utils import ChoiceType
+# _*_coding:utf-8_*_
+from sqlalchemy import create_engine, Table
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint, DateTime
+from sqlalchemy.orm import relationship
+from  sqlalchemy.orm import sessionmaker
+from sqlalchemy import or_, and_
+from sqlalchemy import func
+from sqlalchemy_utils import ChoiceType, PasswordType
 
-Base = declarative_base()
+Base = declarative_base()  # 生成一个SqlORM 基类
 
+engine = create_engine("mysql+mysqldb://root@localhost:3306/test", echo=False)
 
-# 创建多对多表
-host_m2m_remoteuser = Table("host_m2m_remoteuser", Base.metadata,
-                            Column("host_id", Integer, ForeignKey("host.id")),
-                            Column("remoteuser_id", Integer, ForeignKey("remote_user.id"))
-                            )
+BindHost2Group = Table('bindhost_2_group', Base.metadata,
+                       Column('bindhost_id', ForeignKey('bind_host.id'), primary_key=True),
+                       Column('group_id', ForeignKey('group.id'), primary_key=True),
+                       )
 
+BindHost2UserProfile = Table('bindhost_2_userprofile', Base.metadata,
+                             Column('bindhost_id', ForeignKey('bind_host.id'), primary_key=True),
+                             Column('uerprofile_id', ForeignKey('user_profile.id'), primary_key=True),
+                             )
 
-class Host(Base):
-    __tableName__ = "host"
-    id = Column(Integer, primary_key=True)
-    hostname = Column(String(64), unique=True)
-    ip = Column(String(64), unique=True)
-    port = Column(Integer, default=22)
-    remote_users = relationship("RemoteUser", secondary=host_m2m_remoteuser, backref="hosts")
-
-    def __repr__(self):
-        return self.hostname
-
-
-class HostGroup(Base):
-    __tableName__ = "host_group"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(64), unique=True)
-
-    def __repr__(self):
-        return self.name
-
-
-class RemoteUser(Base):
-    __tableName__ = "remote_user"
-    __table_args__ = (UniqueConstraint("auth_type", "username", "password"))   # 联合唯一
-    AuthTypes = [(u"ssh-password", "SSH/Password"),
-                 (u"ssh-key", "SSH/KEY")]
-    id = Column(Integer, primary_key=True)
-    username = Column(String(32))
-    password = Column(String(128))
-    auth_type = Column(ChoiceType(AuthTypes))
-
-    def __repr__(self):
-        return self.username
+Group2UserProfile = Table('group_2_userprofile', Base.metadata,
+                          Column('userprofile_id', ForeignKey('user_profile.id'), primary_key=True),
+                          Column('group_id', ForeignKey('group.id'), primary_key=True),
+                          )
 
 
 class UserProfile(Base):
-    __tableName__ = "user_profile"
-    username = Column(String(32), unique=True)
-    password = Column(String(128))
+    __tablename__ = 'user_profile'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(32), unique=True, nullable=False)
+    password = Column(String(128), unique=True, nullable=False)
+    groups = relationship('Group', secondary=Group2UserProfile)
+    bind_hosts = relationship('BindHost', secondary=BindHost2UserProfile)
 
     def __repr__(self):
-        return self.username
+        return "<UserProfile(id='%s',username='%s')>" % (self.id, self.username)
+
+
+class RemoteUser(Base):
+    __tablename__ = 'remote_user'
+    AuthTypes = [
+        (u'ssh-passwd', u'SSH/Password'),
+        (u'ssh-key', u'SSH/KEY'),
+    ]
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    auth_type = Column(ChoiceType(AuthTypes))
+    username = Column(String(64), nullable=False)
+    password = Column(String(255))
+
+    __table_args__ = (UniqueConstraint('auth_type', 'username', 'password', name='_user_passwd_uc'),)
+
+    def __repr__(self):
+        return "<RemoteUser(id='%s',auth_type='%s',user='%s')>" % (self.id, self.auth_type, self.username)
+
+
+class Host(Base):
+    __tablename__ = 'host'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    hostname = Column(String(64), unique=True, nullable=False)
+    ip_addr = Column(String(128), unique=True, nullable=False)
+    port = Column(Integer, default=22)
+    bind_hosts = relationship("BindHost")
+
+    def __repr__(self):
+        return "<Host(id='%s',hostname='%s')>" % (self.id, self.hostname)
+
+
+class Group(Base):
+    __tablename__ = 'group'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(64), nullable=False, unique=True)
+    bind_hosts = relationship("BindHost", secondary=BindHost2Group, back_populates='groups')
+    user_profiles = relationship("UserProfile", secondary=Group2UserProfile)
+
+    def __repr__(self):
+        return "<HostGroup(id='%s',name='%s')>" % (self.id, self.name)
+
+
+class BindHost(Base):
+    __tablename__ = 'bind_host'.encode()
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    host_id = Column(Integer, ForeignKey('host.id'))
+    remoteuser_id = Column(Integer, ForeignKey('remote_user.id'))
+    host = relationship("Host")
+    remoteuser = relationship("RemoteUser")
+    groups = relationship("Group", secondary=BindHost2Group, back_populates='bind_hosts')
+    user_profiles = relationship("UserProfile", secondary=BindHost2UserProfile)
+
+    __table_args__ = (UniqueConstraint('host_id', 'remoteuser_id', name='_bindhost_and_user_uc'),)
+
+    def __repr__(self):
+        return "<BindHost(id='%s',name='%s',user='%s')>" % (self.id,
+                                                            self.host.hostname,
+                                                            self.remoteuser.username
+
+                                                            )
+class AuditLog(Base):
+    __tablename__ = 'audit_log'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user_profile.id'))
+    bind_host_id = Column(Integer, ForeignKey('bind_host.id'))
+    action_choices = [
+        (0, 'CMD'),
+        (1, 'Login'),
+        (2, 'Logout'),
+        (3, 'GetFile'),
+        (4, 'SendFile'),
+        (5, 'Exception'),
+    ]
+    action_type = Column(ChoiceType(action_choices))
+    cmd = Column(String(255))
+    date = Column(DateTime)
+
+    user_profile = relationship("UserProfile")
+    bind_host = relationship("BindHost")
 
